a808894a8859e63b71de058e2e3608ca1dd3a71d
AllenEule <allenfranzend.eule@gmail.com>
2026-09-03 10:38:54 +0800

    Add files via upload

---
+++ activity6.py
@@ -0,0 +1,15 @@
+#int()
+Y = eval(input("What is your age?"))
+
+print("Your age is", Y)
+print(type(Y))
+
+X = int(input("What number of month is September?"))
+
+print("It is", X)
+print(type(X))
+
+Z = float(input("What is your shoe size?"))
+
+print("It is", Z)
+print(type(Z))
---
+++ activity7.py
@@ -0,0 +1,10 @@
+#assignment operator
+total = 12
+n1 = eval(input("put in a number -"))
+total += n1
+n2 = eval(input("put in a number -"))
+total *= n2
+n3 = eval(input("put in a number -"))
+total /= n3
+
+print("Total is =", total)
