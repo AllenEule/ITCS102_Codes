e52b4214bdd532dc1c842e35dd9eabb9d2f4ad27
AllenEule <allenfranzend.eule@gmail.com>
2026-09-03 10:54:08 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,13 @@
+#Assignment operator
+
+Phones = " " 
+
+phone = input("What is your phone's brand?")
+
+Phones += phone + ", "
+
+phone = input("What else?")
+
+Phones += phone
+
+print("Your Phone's Brands are", Phones)
