0a81e38145633c8568956939cd2529781d08b539
AllenEule <allenfranzend.eule@gmail.com>
2026-09-05 08:27:37 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,34 @@
+#This is the integers one
+#Whereas the money is 1540
+
+money = 1540
+
+a = money // 1000
+money1 = money % 1000
+l = money1 // 500
+money2 = money1 % 500
+ll = money2 // 200
+money3 = money2 % 200
+e = money3 // 100
+money4 = money3 % 100
+n = money4 // 50
+money5 = money4 % 50
+ee = money5 // 20
+money6 = money5 % 20
+u = money6 // 10
+money7 = money6 % 10
+lll = money7 // 5 
+money8 = money7 % 5
+eee = money8 // 1
+money9 = money8 % 1
+
+print("Your current money is", money)
+print("1000 =", a)
+print("500 =", l)
+print("200 =", ll)
+print("100 =", e)
+print("50 =", n)
+print("20 =", ee)
+print("10 =", u)
+print("5 =", lll)
+print("1 =", eee)
